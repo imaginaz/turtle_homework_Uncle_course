a7f3ae30e3a3acd53692a047d96f547e6e2de3c6
imaginaz <39219845+imaginaz@users.noreply.github.com>
2022-03-20 19:30:13 +0700

    Update stars-turtle.py

--- stars-turtle.py
+++ stars-turtle.py
@@ -1,8 +1,29 @@
 #Basic Python Course - Star turtle
 import turtle
+import random
+
 tao = turtle.Pen()
 tao.shape('turtle')
 
-for i in range(5):
-    t.forward(150)
-    t.right(144)
+taosc = turtle.Screen()
+taosc.bgcolor('black')
+
+
+color = ['red','blue','green','yellow','orange','purple']
+random_size = [10,20,30,40,50,60,70,80,90,100]
+
+def Star_Shape(size=50):
+    for i in range(5):
+        tao.forward(size)
+        tao.right(144)
+
+for i in range(100):
+    x = random.randint(-400,400)
+    y = random.randint(-300,300)
+    tao.penup()
+    tao.goto(x,y)
+    tao.pendown()
+    colors = random.choice(color)
+    tao.color(colors)
+    randomsizes = random.choice(random_size)
+    Star_Shape(randomsizes)
